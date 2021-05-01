7d725055ae70570ccca747bfbc872e43c7040748
ais2397 <ayushisharma@am.students.amrita.edu>
2021-05-01 12:21:49 +0530

    Add dumb fuzzing class

--- core/dumb_fuzzing.py
+++ core/dumb_fuzzing.py
@@ -1,4 +1,4 @@
-#from core.logger import get_logger
+from core.logger import get_logger
 
 import socket
 import sys
@@ -7,69 +7,69 @@ import struct
 import time
 import logging
 
-HOST = '127.0.0.1'    
-dest_port = 5020       
-sock = None
-dumbflagset = 0;
-logging.basicConfig(filename='./fuzzer.log', filemode='a', level=logging.DEBUG, format='[%(asctime)s][%(levelname)s] %(message)s')
+class DFuzz(object):
 
-def create_connection(dest_ip, port):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as msg:
-        sys.stderr.write("[ERROR] %s\n" % msg[1])
-        sys.exit(1)
+    def __init__(self, r0obj):
+        self.r0obj = r0obj
+        self.HOST = "127.0.0.1"
+        self.dest_port = 5020
+        self.verbosity = self.r0obj.log_level
 
-    HOST = dest_ip
-    try:
-        sock.settimeout(0.5)
-        sock.connect((HOST, dest_port))
-    except socket.error as msg:
-        logging.exception("Connection Failed!")
-    else:
-        logging.info("Connected to Server: %s" % dest_ip)
+        self.logger = get_logger("Dumbfuzz", self.verbosity)
 
-    return sock
+    def create_connection(self, dest_ip, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as msg:
+            self.logger.error("[-] Unable to establish socket connection")
+            sys.exit(1)
 
+        self.HOST = dest_ip
+        try:
+            sock.settimeout(0.5)
+            sock.connect((self.HOST, self.dest_port))
+        except socket.error as msg:
+            self.logger.error(msg)
+        else:
+            self.logger.info("[+] Connected to Server: %s" % dest_ip)
 
-def hexstr(s):
-    return '-'.join('%02x' % ord(c) for c in s)
-
-
-def dumb_fuzzing(dest_ip):
-  sock = create_connection(dest_ip, dest_port)
-  length1 = 0
-  length2 = 6
-  unitID = 1
-
-  for transID1 in range(0,0xff):
-    for protoID1 in range(0,0xff):
-      for functionCode in range(0,0xff):
-        for functionData1 in range(0,0xff):
-          for functionData2 in range(0,0xff):
-            TotalModbusPacket =  ""
-            TotalModbusPacket += struct.pack(">H", transID1)
-            TotalModbusPacket += struct.pack(">H", protoID1)
-            TotalModbusPacket += struct.pack(">B", length1)
-            TotalModbusPacket += struct.pack(">B", length2)
-            TotalModbusPacket += struct.pack(">B", unitID)
-            TotalModbusPacket += struct.pack(">B", functionCode)
-            TotalModbusPacket += struct.pack(">H", functionData1)
-            TotalModbusPacket += struct.pack(">H", functionData2)
-            logging.debug("%s" % hexstr(TotalModbusPacket))
-            try:
-              sock.send(TotalModbusPacket)
-              print >>sys.stderr,'received: %s'% hexstr(sock.recv(1024))
-            except socket.timeout:
-              logging.exception("Sending Timed Out!")
-            except socket.error:
-              #logging.exception("Sending Failed!")
-              sock.close()
-              sock = create_connection(dest_ip, dest_port)
-              #logging.info("Try to Reconnect...")
-            else:
-              logging.debug("Sent Packet: %s" % hexstr(TotalModbusPacket))
-              print("Sent: %s" % hexstr(TotalModbusPacket))
+        return sock
 
 
-dumb_fuzzing(HOST)
+    def hexstr(self, s):
+        return '-'.join('%02x' % ord(c) for c in s)
+
+
+    def dumb_fuzzing(self):
+      sock = self.create_connection(self.HOST, self.dest_port)
+      length1 = 0
+      length2 = 6
+      unitID = 1
+
+      for transID1 in range(0,0xff):
+        for protoID1 in range(0,0xff):
+          for functionCode in range(0,0xff):
+            for functionData1 in range(0,0xff):
+              for functionData2 in range(0,0xff):
+                TotalModbusPacket = struct.pack(">H", transID1)
+                TotalModbusPacket += struct.pack(">H", protoID1)
+                TotalModbusPacket += struct.pack(">B", length1)
+                TotalModbusPacket += struct.pack(">B", length2)
+                TotalModbusPacket += struct.pack(">B", unitID)
+                TotalModbusPacket += struct.pack(">B", functionCode)
+                TotalModbusPacket += struct.pack(">H", functionData1)
+                TotalModbusPacket += struct.pack(">H", functionData2)
+                self.logger.debug("[+] Packet sent: %s" % str(TotalModbusPacket))
+                try:
+                  sock.send(TotalModbusPacket)
+                  print >>sys.stderr,'received: %s'% self.hexstr(sock.recv(1024).decode("utf-8"))
+                except socket.timeout:
+                  self.logger.warning("Sending Timed Out!")
+                except socket.error:
+                  self.logger.warning("Sending Failed!")
+                  sock.close()
+                  sock = self.create_connection(self.HOST, self.dest_port)
+                  #logging.info("Try to Reconnect...")
+                else:
+                  self.logger.debug("[+] Sent Packet: %s" % self.hexstr(TotalModbusPacket.decode("utf-8")))
+
--- r0fuzz.py
+++ r0fuzz.py
@@ -1,7 +1,7 @@
 from core.logger import get_logger
 
 from core.extract import Extractor
-#from core.dumb_fuzzing import *
+from core.dumb_fuzzing import DFuzz
 
 # Default imports 
 import argparse
@@ -12,26 +12,42 @@ from termcolor import cprint
 from pyfiglet import figlet_format
 
 class r0fuzz(object):
+    
+    supported_protocol = ["modbus"]
 
-    def __init__(self, seed, protocol, dumbool, log_level):
-        self.seed = os.path.join(os.getcwd(), seed)
+    def __init__(self, seed, protocol, dumbool, genbool, mutbool, log_level):
         self.protocol = protocol
-        self.dumb = dumbool
+        self.dumbool = dumbool
+        self.genbool = genbool
+        self.mutbool = mutbool       
         self.log_level = log_level
-
-        self.extractor = Extractor(self)
+        self.seed = os.path.join(os.getcwd(), seed)
 
         if not self._sanity_check():
             logging.critical("[+] r0fuzz failed to init")
             sys.exit(-1)
 
+        if self.dumbool:
+            self.dfuzz = DFuzz(self)
+
+        if self.mutbool:
+            
+            self.extractor = Extractor(self)
+
     def _sanity_check(self) -> bool:
         """Verify the arguments passed"""
-
-        if not os.path.isfile(self.seed):
-            logging.error("[+] The seed file is not found at %s", self.seed)
+        
+        if self.protocol.lower() not in self.supported_protocol:
+            logging.error("[-] %s protocol is not supported", self.protocol)
             return False
-        logging.debug("[+] The input file is at %s", self.seed)
+        logging.debug("[+] Fuzzing %s protocol", self.protocol)
+
+        if self.mutbool==True:
+            if not os.path.isfile(self.seed):
+                logging.error("[-] The seed file is not found at %s", self.seed)
+                return False
+            logging.debug("[+] The input file is at %s", self.seed)
+
         return True
 
 def main():
@@ -44,22 +60,25 @@ def main():
 
     parser.add_argument("-s", "--seed", help="sample input file", type=str, required=True)
     parser.add_argument("-t", "--target", help="target protocol", type=str, required=True)
+    parser.add_argument("-m", "--mutate", help="Apply mutation based method", action="store_true")
+    parser.add_argument("-g", "--generate", help="Apply generation based method", action="store_true")
     parser.add_argument("-d", "--dumb", help="Dumb fuzz the target", action="store_true")
     parser.add_argument("-v", "--verbosity", help="Log level", action="count")
     args = parser.parse_args()
 
     logging = get_logger("r0fuzz", args.verbosity)
 
-    r0obj = r0fuzz(args.seed, args.target, args.dumb, args.verbosity)
+    r0obj = r0fuzz(args.seed, args.target, args.dumb, args.generate, args.mutate, args.verbosity)
+    
+    if r0obj.mutbool:
+        r0obj.extractor.generate_fields()
+        logging.info('[+] Generated fields')
 
-    r0obj.extractor.generate_fields()
-    logging.info('[+] Generated fields')
-
-    if r0obj.dumb:
-        print("Dumb fuzz")
-    else:
-        print("grammar fuzz")
-
+    if r0obj.dumbool:
+        if not r0obj.dfuzz.dumb_fuzzing():
+            logging.error("[-] Failed to dumb fuzz the target")
+            sys.exit(-1)
+    
 if __name__ == "__main__":
     logging = None
     main()
